3e76d494ed307ea87fbdb7791d9a78e2403cc840
OndrejZ <srixon@post.cz>
2022-10-24 18:17:53 +0200

    Working todolist built with react

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
   const [newTask, setNewTask] = useState("");
   const [todoList, setTodoList] = useState([]);
 
-  const storeNewTask =()=>{
+  const storeNewTask =(e)=>{
+    
+    let form =document.querySelector('#todoList');
+    form.firstElementChild.value = "";
     const task = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length -1].id + 1,
       taskName: newTask,
@@ -39,8 +42,11 @@ function App() {
 
   return(
     <div className="app" style={{textAlign: 'center'}}>
-      <input onChange={(e)=>{setNewTask(e.target.value)}}/>
+      <h1>My TO DO LIST</h1>
+      <div id="todoList">
+      <input name="taskBar" onChange={(e)=>{setNewTask(e.target.value)}}/>
       <input type="submit" onClick={storeNewTask}/>
+      </div>
       <div>
         {todoList.map((oneTask, key)=>{
           return <Task key={key} taskName={oneTask.taskName} id={oneTask.id} deleteTask={deleteTask} taskStatus = {taskStatus} completed={oneTask.completed}/>
